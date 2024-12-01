f63f6f6b558758b992c371ffa34e7d06db70723e
Nan.MacMini <nanwang2016@gmail.com>
2024-12-01 15:13:46 +0800

    Update icon url

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733036971',resources: [
+    cacheName: 'chirpy-1733037169',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
