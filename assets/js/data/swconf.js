e0e15f91cad269534fae2c0b1d01cfd9f1340683
Nan.MacMini <nanwang2016@gmail.com>
2024-12-01 14:45:54 +0800

    Setup

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733031869',resources: [
+    cacheName: 'chirpy-1733035520',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
