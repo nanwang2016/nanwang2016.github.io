7455ce870b8303f3a8bad1a2c93c1eb701672568
Nan.MacMini <nanwang2016@gmail.com>
2024-12-01 14:53:11 +0800

    Update logo

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733035520',resources: [
+    cacheName: 'chirpy-1733035971',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
